dbad8f677f9663bf057deeb22123f353f282ff7a
johnfikiri01-oss <johnfikiri01@gmail.com>
2026-02-12 22:35:28 +0200

    Update crew.js

--- Digix/crew.js
+++ Digix/crew.js
@@ -111,7 +111,7 @@ async function connectToWhatsapp(handleMessage) {
                 }
 
                 console.log(`🔄 Requesting pairing code for ${number}`);
-                const code = await sock.requestPairingCode(number, 'JOHNNY');
+                const code = await sock.requestPairingCode(number, 'JOHNNYSW');
                 console.log('📲 Pairing Code:', code);
                 console.log('👉 Enter this code on your WhatsApp app to pair.');
 
